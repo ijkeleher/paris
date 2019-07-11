12d16e1478b4d1731f0b09f4120006d5df3de2f1
Isaac Keleher <isaac.keleher@gmail.com>
2019-07-11 10:32:24 +0200

    Added sitemap, robot and manifest

--- react-module/gatsby-config.js
+++ react-module/gatsby-config.js
@@ -1,8 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby website`,
+    title: `My Awesome Blog`,
+    siteUrl: `https://www.keleher.me`
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        plugins: [
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.keleher.me',
+        sitemap: 'https://www.keleher.me/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,6 +31,24 @@ module.exports = {
       options: {
         plugins: [
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
   ],
